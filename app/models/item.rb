6e703717e75f4e1e4d299d2a84bd26f00355fa6a
yukarikariri <doggieholic3015@gmail.com>
2021-07-14 12:08:27 +0900

    ActiveHashの導入、モデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :lead_time
+  belongs_to :user
+
+  with_options presence: true
+    validates :item_name
+    validates :item_description
+    validates :item_price
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :charge_id
+    validates :prefecture_id
+    validates :leadtime_id
+  end
 end
