cfa26bc37b97151f6a63277f61d00abb5233f472
yukarikariri <doggieholic3015@gmail.com>
2021-07-14 16:24:14 +0900

    商品情報データベース保存の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :condition_id, :charge_id, :prefecture_id, :leadtime_id, :item_price).merge(user_id: current_user.id)
   end
 end
