9fc4f0bfdcecd8f81b7813c55ce264982c720396
yukarikariri <doggieholic3015@gmail.com>
2021-07-15 09:00:35 +0900

    販売手数料や販売利益の表示機能実装

---
+++ app/javascript/item.js
@@ -0,0 +1,17 @@
+function input (){
+ 
+  const priceInput = document.getElementById("item-price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+  const feeRate = 0.1;
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue)
+    const taxPriceValue = Math.floor(inputValue*feeRate)
+    taxPrice.innerHTML = taxPriceValue.toLocaleString('ja-JP')
+    profit.innerHTML = (inputValue - taxPriceValue).toLocaleString('ja-JP')
+  });
+};
+
+window.addEventListener('load', input);
