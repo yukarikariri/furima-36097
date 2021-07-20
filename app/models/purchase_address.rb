de3bef8f124e7ec9fafaa2461fa0a2b4470acbe5
yukarikariri <doggieholic3015@gmail.com>
2021-07-20 12:53:19 +0900

    提出前最終動作チェック時の修正
    バリデーションによるエラーメッセージが重複して表示されたため、修正

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -9,8 +9,8 @@ class PurchaseAddress
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
     validates :town
     validates :house_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }
-    validates :phone_number, length: { minimum: 10, message: 'is too short' }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number' }, length: { minimum: 10, message: 'is too short' }
+    # validates :phone_number, length: { minimum: 10, message: 'is too short' }
     validates :user_id
     validates :item_id
   end
